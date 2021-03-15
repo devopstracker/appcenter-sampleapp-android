d50be52bc69449005a4ee2b4249e2b6eaf519e77
Sundeep Bobba <sundeep.bobba@wnco.com>
2021-03-15 15:31:52 -0500

    Adding secret

--- app/src/main/java/ms/appcenter/sampleapp/android/MainActivity.java
+++ app/src/main/java/ms/appcenter/sampleapp/android/MainActivity.java
@@ -44,7 +44,7 @@ public class MainActivity extends AppCompatActivity {
                     Analytics.class, Crashes.class, Distribute.class);
         } else {
             // Otherwise use the hardcoded string value here
-            AppCenter.start(getApplication(), "<APP SECRET HERE>",
+            AppCenter.start(getApplication(), "32283549-e624-4aef-81eb-1e2aab16a267",
                     Analytics.class, Crashes.class, Distribute.class);
         }
 
